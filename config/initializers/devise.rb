57271244c0a0f45f7d080497855614c9f6f767de
kataikko <vincent.kataikko@uniklinik-freiburg.de>
2024-10-13 20:44:21 +0200

    feat: devise_ldap_authenticatable setup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,11 +15,12 @@ Devise.setup do |config|
   config.ldap_update_password = false
   config.ldap_config = "#{Rails.root}/config/ldap.yml"
   config.ldap_check_group_membership = false
-  config.ldap_check_group_membership_without_admin = false
+  # config.ldap_check_group_membership_without_admin = true
   config.ldap_check_attributes = false
-  config.ldap_check_attributes_presence = false
+  config.ldap_check_attributes_presence = true
   config.ldap_use_admin_to_bind = ENV['DEVISE_LDAP_LOGGER_ADMIN_BIND']
-  # config.ldap_ad_group_check = false
+  config.ldap_ad_group_check = false
+  config.ldap_auth_username_builder = Proc.new() {|attribute, login, ldap| "#{attribute}=#{login}"}
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
